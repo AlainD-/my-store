4db09e7384e602668fdc55a542b9c1313bc31a3a
Alain D'EURVEILHER <alain.deurveilher@infrabel.be>
2021-12-15 22:31:08 +0100

    add login route and component

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -1,19 +1,51 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { RegistrationInput } from '../models/registration-input';
-import { User, UserI } from './../models/user';
+import { User, UserI } from '../models/user';
+import { AuthResponse } from '../models/auth-response';
+import { Credentials } from '../models/credentials';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
+  private currentUser: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
+  readonly currentUser$: Observable<User | null> = this.currentUser.asObservable();
+
   constructor(private http: HttpClient) {}
+
+  login$(data: Credentials): Observable<AuthResponse> {
+    return this.http
+      .post<{ token: string; user: UserI }>(`${environment.apiUrl}/authenticate`, data)
+      .pipe(map(({ token, user }) => ({ token, user: new User(user) })));
+  }
+
+  logout(): void {
+    this.removeTokenSession();
+    this.currentUser.next(null);
+  }
 
   register$(data: RegistrationInput): Observable<User> {
     return this.http
       .post<UserI>(`${environment.apiUrl}/users`, data)
       .pipe(map((user: UserI) => new User(user)));
+  }
+
+  private removeTokenSession(): void {
+    localStorage.removeItem('TS_TOKEN');
+  }
+
+  saveToken(token: string): void {
+    this.setTokenSession(token);
+  }
+
+  setCurrentUser(user: User | null): void {
+    this.currentUser.next(user);
+  }
+
+  private setTokenSession(token: string): void {
+    localStorage.setItem('TS_TOKEN', token);
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Credentials } from '../core/models/credentials';
+import { AuthenticationService } from '../core/services/authentication.service';
+import { NotificationService } from '../core/services/notification.service';
+import { AuthResponse } from './../core/models/auth-response';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit, OnDestroy {
+  @ViewChild('form', { static: false }) form!: NgForm;
+  credentials!: Credentials;
+  submitted = false;
+  private subscriptions: Subscription = new Subscription();
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private notificationService: NotificationService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.credentials = { email: '', password: '' };
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscriptions) {
+      this.subscriptions.unsubscribe();
+    }
+  }
+
+  onLogin(): void {
+    this.submitted = true;
+
+    if (this.form.valid) {
+      this.subscriptions.add(
+        this.authenticationService.login$(this.form.value as Credentials).subscribe({
+          next: async ({ token, user }: AuthResponse) => {
+            this.authenticationService.saveToken(token);
+            this.authenticationService.setCurrentUser(user);
+            await this.router.navigate(['/']);
+          },
+          error: (error: unknown) => {
+            this.notificationService.notifyError(error);
+          },
+        })
+      );
+    }
+  }
+}
