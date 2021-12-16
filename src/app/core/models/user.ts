fcba822354a47961082903835c76e91b2e52a87f
Alain D'EURVEILHER <alain.deurveilher@infrabel.be>
2021-12-16 16:31:47 +0100

    Fix User model with isAdmin property

--- src/app/core/models/user.ts
+++ src/app/core/models/user.ts
@@ -3,6 +3,7 @@ export interface UserI {
   email: string;
   firstName: string;
   lastName: string;
+  isAdmin: boolean | number;
 }
 
 export class User implements UserI {
@@ -10,12 +11,14 @@ export class User implements UserI {
   email: string;
   firstName: string;
   lastName: string;
+  isAdmin: boolean;
 
-  constructor({ id, email, firstName, lastName }: UserI) {
+  constructor({ id, email, firstName, lastName, isAdmin }: UserI) {
     this.id = id;
     this.email = email;
     this.firstName = firstName;
     this.lastName = lastName;
+    this.isAdmin = typeof isAdmin === 'number' ? isAdmin === 1 : isAdmin;
   }
 
   get fullName(): string {
